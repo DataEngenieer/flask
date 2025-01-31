eaddb617101126bcf676e740007756610997f247
Jhon Moreno Ruiz <127630511+DataEngenieer@users.noreply.github.com>
2025-01-31 11:43:21 -0500

    Update conexionBD.py

--- conexion/conexionBD.py
+++ conexion/conexionBD.py
@@ -43,25 +43,25 @@ def connectionBD_invxx():
         print(f"No se pudo conectar: {error}")
 
 def connectionBD_inv():
-    try:
-        connection = mysql.connector.connect(
-            host="94.74.75.248",
-            user="rpa_inventario_2",
-            passwd="mIh23A7EB3yE5A3uXApOgEwOFOkI68",
-            database="bd_claro",
-            port=3306,
-            charset='utf8mb4',
-            #collation='utf8mb4_general_ci',# Solo usa charset
-            raise_on_warnings=True
-        )
-        if connection.is_connected():
-            # Conexión exitosa
-            #print(connection)
-            return connection
+    charsets = ['utf8mb4', 'utf8']  # Lista de charset a probar
 
-    except mysql.connector.Error as error:
-        print(f"No se pudo conectar: {error}")
-        return None
-    except Exception as e:
-        print(f"Error general: {e}")
-        return None
+    for charset in charsets:
+        try:
+            connection = mysql.connector.connect(
+                host="94.74.75.248",
+                user="rpa_inventario_2",
+                passwd="mIh23A7EB3yE5A3uXApOgEwOFOkI68",
+                database="bd_claro",
+                port=3306,
+                charset=charset,
+                raise_on_warnings=True
+            )
+            if connection.is_connected():
+                print(f"Conectado con charset: {charset}")
+                return connection
+
+        except mysql.connector.Error as error:
+            print(f"Fallo con charset {charset}: {error}")
+
+    print("No se pudo conectar a la base de datos.")
+    return None
